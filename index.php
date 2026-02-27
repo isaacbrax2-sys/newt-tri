e57b3c6fff0a5baf506f3bf373ff864b1d4003b3
isaacbrax2-sys <isaacbrax2@gmail.com>
2026-02-27 08:41:11 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	 <title>KFGC-home</title> 
+	<link rel="stylesheet" type="text/css" href="_includes/assets/css/index.css">
+	<style type="text/css">
+		body {background: black;margin-left: 100px;margin-right: 230px;}
+	</style>
+</head>
+<body>
+<!--Main Content-->
+<div class="navbar-custom">
+	
+	<div class="class">
+		<p><a id="top"></a></p>
+		<header>
+			<p>||Mukiibi Isaac</p>
+				<nav>
+					<?php include'_includes/index-works/header.php';?>
+				</nav>
+		</header>
+	</div>
+
+
+<main>
+<!---First Division-->
+<div class="welcom">
+	<p>You're highly welcome to this website..</p>
+</div>
+	<section class="abts" style="columns: 2;padding-left: 150px;">
+		<div class="one">
+			<h3>Vision</h3>
+			<p>
+				A Christ centerd and a horistically transforming chuch for community development.
+			</p>	
+		</div>
+
+		<div class="two">
+			<h3>Mission:</h3>
+			<p>
+				To positively impact and transform lives of people of all ages through intentional evangelism, discipleship, mentoring and social welfare promotion.
+			</p>
+		</div>
+		<div class="three">
+			<h3>Core Values</h3>
+			<p>
+				<ul>
+					<li>Integrity and Character Building</li>
+					<li>Team Work</li>
+					<li>Sustainable Development</li>
+					<li>Family life Development</li>
+					<li>Bible based ethics</li>
+					<li>Christ centered</li>
+				</ul>
+			</p>
+		</div>
+	</section>	
+
+<!--registering option-->
+	<section>
+		<p>In order to continue with exploring more and keeping track of updates, you can just choose to become part of this fellowship program by signing up..<br><button type="button" style="width: 150px;border-radius: 15px;text-align: center;font-weight: bold;height: 30px;"class="btn btn-secondary bg-primary disabled"onclick="location.href = '_includes/pag/register-page.php'">Sign Up</button></p>
+	</section>
+</main>
+</div>
+
+
+<!--Footer Content-->
+<footer>
+	<?php include'_includes/pag/footer.php'; ?>	
+</footer>
+
+</body>
+</html>
+
